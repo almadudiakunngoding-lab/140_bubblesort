0d7efbb64c71b9e0e57a22337a25459b354acbbf
almaudud <almadudiakunngoding@gmail.com>
2026-03-05 11:41:42 +0700

    menambahkan Element Array yang telah tersusun

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -19,9 +19,47 @@ void input() {          // Procedur untuk input
     cout << "====================" << endl;
     cout << "Masukkan Elemen Array" <<endl;
     cout << "====================" << endl;
-    
+
     for (int i = 0; i < n; i++) {   //lopping dengan  i dimulau dari 0
         cout << "data ke -" << (i + 1) << ": ";     // output ke layar
-        cin >> arr[i];                          //input dari pengguna
+        cin >> arr[i];                     //input dari pengguna
         }
 }
+
+void bubblesortArray() {   //procedur untnk mengurutkan array dengan  
+    int pass = 1; // step 1
+    
+    do {
+        for (int j = 0; j <= n -1 - pass; j++) { //step 2
+            if(arr[j] > arr[j + 1]) {   // step 3 
+              int temp;
+              temp = arr[j];
+              arr[j] = arr[j + 1];
+              arr[j + 1] = temp;
+             }
+        }
+        pass = pass + 1; // step 4
+
+        cout << "\npass " << pass -1 << ": ";// number of pass
+        for (int k = 0; k < n; k++) {
+            cout << arr[k] << " ";
+
+        }
+        cout << endl;
+
+    } while (pass <= n - 1); // step 5
+    
+}
+    void display() {
+        cout << endl;
+        cout << "==================================" << endl;
+        cout << "Element Array yang telah tersusun" << endl;
+        cout << "==================================" << endl;
+        cout << endl;
+        for (int j = 0; j < n; j++) {
+            cout << arr[j]; // menampilkan array
+            if (j < n - 1) {
+                cout << "--> ";
+            }
+        }
+   }
